45ceaceb9c101a86dcd385ca43a1b6865edea5dd
Jennifer <imjennyyang@gmail.com>
2023-06-08 00:59:24 +0800

    Update start screen

--- musicGame/startScreen.lua
+++ musicGame/startScreen.lua
@@ -1,55 +1,55 @@
-local composer = require("composer") -- 用於創建場景
+local composer = require("composer") 
 local scene = composer.newScene()
+local sp_music = audio.loadStream("images/shoot.wav")
+local enter = display.newText("Touch to enter the game", 170, 310, "images/as.ttf", 13)
 
 function scene:create(event)
     local sceneGroup = self.view
-	
-    -- 設定背景
-	local background = display.newImageRect("images/Floral.png", 1000, 480)
+	local backgroundmusic = audio.loadStream("images/mbgm.mp3")
+    audio.play(backgroundmusic, { channel = 1, loops = 10 })
+    audio.setVolume(0.2, { channel = 1 })
+
+	local background = display.newImageRect("images/234.jpg", 1000, 480)
 	background.x=150
-	background.y=240
+	background.y=260
     sceneGroup:insert(background)
+	sceneGroup:insert(enter)
+	
+	local function flash()
+	local function flash2()
+	transition.to(enter, {time=750, x=enter.x, y=enter.y,alpha=0, rotation=0,onComplete=flash}) 
+	end
+	transition.to(enter, {time=750, x=enter.x, y=enter.y,alpha=1, rotation=0,onComplete=flash2})
+	end
+	transition.to(enter, {time=750, x=enter.x, y=enter.y,alpha=0, rotation=0,onComplete=flash})
 
-    -- 顯示遊戲標題
-    local gameTitle = display.newImageRect("images/title.png", 200, 100)
-	gameTitle.x=-170
-	gameTitle.y=120
+	
+	local gameTitle = display.newImageRect("images/slogan2.png", 393, 100)
+	gameTitle.x=163
+	gameTitle.y=224
     sceneGroup:insert(gameTitle)
+	
+	local gameTitle2 = display.newImageRect("images/slogan.png", 393, 100)
+	gameTitle2.x=158
+	gameTitle2.y=222
+    sceneGroup:insert(gameTitle2)
 
-    -- 開始遊戲按鈕
-    local startButton = display.newRect(display.contentCenterX, display.contentCenterY, 200, 80)
-    startButton:setFillColor(0.4, 0.4, 0.8)
-    sceneGroup:insert(startButton)
-
-    local startButtonText = display.newText({
-        text = "開始遊戲",
-        x = display.contentCenterX,
-        y = display.contentCenterY,
-        width = 180,
-        font = native.systemFontBold,
-        fontSize = 36
-    })
-    startButtonText:setFillColor(1, 1, 1)
-    sceneGroup:insert(startButtonText)
-
-    -- 點擊開始遊戲按鈕的事件處理函式
     local function startGame(event)
         if event.phase == "ended" then
-            -- 在這裡執行開始遊戲的相關邏輯
-            -- 例如切換到遊戲場景、載入遊戲資源等
+			audio.play(sp_music, { channel = 3, loops = 0})
+			audio.setVolume(0.05, { channel = 3 })
+			Runtime:removeEventListener( "touch", startGame )
 			composer.removeScene("scene")
-            composer.gotoScene("select", { effect = "fade", time = 500 })
+            composer.gotoScene("select", { effect = "fade", time = 800 })
         end
     end
 
-    startButton:addEventListener("touch", startGame)
+    Runtime:addEventListener("touch", startGame)
 end
-
 
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
--- "scene:destroy()"
 
 return scene
