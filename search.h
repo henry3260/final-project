d716aa928177badd39b00b23968d7b8247e3654c
Jennifer <imjennyyang@gmail.com>
2023-06-12 22:54:26 +0800

    Update search functions

--- search.h
+++ search.h
@@ -1,11 +1,12 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
-struct music *search_artist(struct music *head, const char *target_artist);
-struct music *search_title(struct music *head, const char *target_title);
+struct music *search_artist(struct music *head, wchar_t *target_artist);
+struct music *search_title(struct music *head,  wchar_t *target_title);
 int count(struct music *head);
 void printList(struct music *head);
 int binary_search(struct music *head, float target, float arr[], int nums);
+int my_strcmp(const wchar_t* str1, const wchar_t* str2);
 
 #endif
 
