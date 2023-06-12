6ca2e6dcbc885264cf6bb3e96f39b6993acd85e6
Jennifer <imjennyyang@gmail.com>
2023-06-12 22:50:24 +0800

    Update queue

--- queue.h
+++ queue.h
@@ -7,5 +7,8 @@ struct Queue *createQueue();
 void enQueue(struct Queue *q, wchar_t *new_title, wchar_t *new_artist, int new_date[3], float new_length, wchar_t *new_link);
 void deQueue(struct Queue *q);
 void printQueue(struct Queue *q);
+int getSize(struct Queue *q);
+struct music *dequeueAtIndex(struct Queue *q, int index);
+void shufflePlaylist(struct music **head);
 
 #endif
