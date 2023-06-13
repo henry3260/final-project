ecd74e9d96b8aa5412fe4b2198ac74615d390fbd
弘逸 <108824346+henry3260@users.noreply.github.com>
2023-06-13 08:55:38 +0800

    Update search.h

--- search.h
+++ search.h
@@ -4,7 +4,6 @@
 struct music *search_artist(struct music *head, wchar_t *target_artist);
 struct music *search_title(struct music *head,  wchar_t *target_title);
 int count(struct music *head);
-void printList(struct music *head);
 int binary_search(struct music *head, float target, float arr[], int nums);
 int my_strcmp(const wchar_t* str1, const wchar_t* str2);
 
